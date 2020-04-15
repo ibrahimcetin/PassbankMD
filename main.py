c9ffbd5cec95c692ca40134c4ac50ab9ca96a53b
Ibrahim Cetin <cetinibrahim@yahoo.com>
2020-04-15 14:55:43 +0300

    sqlite sorting updated

--- main.py
+++ main.py
@@ -584,7 +584,7 @@ class MainScreen(Screen):
         self.chars = ["q","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","z","x","c","v","b","n","m","1","2","3","4","5","6","7","8","9","0","Q","W","E","R","T","Y","U","I","O","P","A","S","D","F","G","H","J","K","L","Z","X","C","V","B","N","M","!","?","&","#","(",")","_","-","@","$",".","+","*", "/"]
 
     def getAccounts(self):
-        self.cursor.execute("SELECT site,email,username FROM accounts ORDER BY site")
+        self.cursor.execute("SELECT site,email,username FROM accounts ORDER BY site COLLATE NOCASE ASC")
         self.accounts = self.cursor.fetchall()
 
     def setAccounts(self):
